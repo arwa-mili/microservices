96ce75e6d3189196fbd4f3eee33c9010a50a1363
ARWA MILI <mili.arwa.ma@gmail.com>
2023-07-16 00:56:42 +0100

    authentication with keycloak
    +configurer adapter problem to fix

---
+++ register-discovery/src/main/java/discoveryserver/registerdiscovery/config/securityConfig.java
@@ -0,0 +1,50 @@
+package discoveryserver.registerdiscovery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig  {
+    @Value("${eureka.username}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        //authenticationManagerBuilder.inMemoryAuthentication()
+                //.passwordEncoder(BCryptPasswordEncoder)
+                //.withUser(username).password(password)
+                //.authorities("USER");
+
+        //}
+
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+
+}
